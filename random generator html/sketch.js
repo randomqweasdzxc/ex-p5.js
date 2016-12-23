bd1e769d379125b95d081086137f0d207fdae016
randomqweasdzxc <vinhoecola@gmail.com>
2016-12-23 02:43:21 +0000

    sketch.js
    how to set the letters inside the squarejs to only change after hitting a key

--- random generator html/sketch.js
+++ random generator html/sketch.js
@@ -4,6 +4,8 @@ var x = 100;
 var y = 100;
 var bgcolor;
 var button;
+var slider;
+var input;
 
 function setup() {
   canvas = createCanvas(200, 200);
@@ -14,22 +16,20 @@ function setup() {
   h1.position(500,200);
   button = createButton("go..go..go");
   button.mousePressed(changeColor);
+  slider = createSlider(10, 100, 47);
+  input = createInput('Random');
 }
 function changeColor() {
   bgcolor = color(random(0, 100));
   h1.html("Now I Will Show Random Numbers")
   n1 = createP("Click again for New Random Number " + random(0, 100));
 }
-/*
-function mousePressed() {
-  h1.html("Now I Will Show Random Numbers")
-  n1 = createP("Click again for New Random Number " + random(0, 100));
-}
-*/
+
 function draw() {
   background(bgcolor);
   fill(255, 0, 0);
-  rect(x, y, 50, 50);
+  rect(x, y, slider.value(), slider.value());
   h1.position(x, y);
   x += random(-3, 3);
+  text(input.value(), 10, 20)
 }
